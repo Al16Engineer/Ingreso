a738bab70b37b4b37c740d2484da89953f09f3e4
Al16Engineer <danizanches.da@gmail.com>
2025-05-13 17:03:27 -0500

    Add files via upload

---
+++ js/presentacion.js
@@ -0,0 +1,21 @@
+const presentacionesPorModelo = {
+    'UNIDAD': [
+        { value: '1', text: 'UNIDAD', factor: 1 },
+    ],
+    'PACA': [
+        { value: '12', text: 'PACA x12', factor: 12 },
+        { value: '24', text: 'PACA x24', factor: 24 }
+    ],
+    'KILO': [
+        { value: '1', text: 'KILO x1', factor: 1 },
+        { value: '5', text: 'KILO x5', factor: 5 },
+        { value: '10', text: 'KILO x10', factor: 10 },
+        { value: '25', text: 'KILO x25', factor: 25 }
+    ],
+    'LOTE': [
+        { value: '1', text: 'LOTE', factor: 1 }
+    ],
+    'PAQUETE': [
+        { value: '12', text: 'PAQUETE x12', factor: 12 }
+    ]
+};
